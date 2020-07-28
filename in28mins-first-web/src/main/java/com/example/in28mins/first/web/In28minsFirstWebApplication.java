6cacc14d5974d6c50842ac6cd65e42095544b977
Maureen Josephine <m.akelo@carepay.com>
2020-07-28 14:51:40 +0300

    initial commit

---
+++ in28mins-first-web/src/main/java/com/example/in28mins/first/web/In28minsFirstWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.in28mins.first.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28minsFirstWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(In28minsFirstWebApplication.class, args);
+	}
+
+}
---
+++ in28mins-first-web/src/test/java/com/example/in28mins/first/web/In28minsFirstWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.in28mins.first.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In28minsFirstWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
