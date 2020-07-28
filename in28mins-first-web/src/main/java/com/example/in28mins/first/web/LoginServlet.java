2ee9676c51aa2972621d620b99b3717eec532e98
JosephineAkello <Josephineakello2030@gmail.com>
2020-07-28 17:00:50 +0300

    add doPost method

--- in28mins-first-web/src/main/java/com/example/in28mins/first/web/LoginServlet.java
+++ in28mins-first-web/src/main/java/com/example/in28mins/first/web/LoginServlet.java
@@ -13,4 +13,14 @@ public class LoginServlet extends HttpServlet{
       )
     }
 
+    protected void doPost(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, java.io.IOException{
+        request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(
+                request,response);
+        request.setAttribute("name", request.getParameter("name");
+        request.setAttribute("password", request.getParameter("password");
+      )
+    }
+
 }
