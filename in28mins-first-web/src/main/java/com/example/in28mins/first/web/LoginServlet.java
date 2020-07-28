84b106b9e50445bc8fd5ce3e462b6e9b1220cc81
JosephineAkello <Josephineakello2030@gmail.com>
2020-07-28 17:31:40 +0300

    add error message

--- in28mins-first-web/src/main/java/com/example/in28mins/first/web/LoginServlet.java
+++ in28mins-first-web/src/main/java/com/example/in28mins/first/web/LoginServlet.java
@@ -3,6 +3,8 @@ package com.example.in28mins.first.web;
 @WebServlet(urlPatterns ="/login.do")
 public class LoginServlet extends HttpServlet{
 
+    private UserValidationService service = new UserValidationService service();
+
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response)
         throws ServletException, java.io.IOException{
@@ -14,9 +16,23 @@ public class LoginServlet extends HttpServlet{
     protected void doPost(HttpServletRequest request,
                          HttpServletResponse response)
             throws ServletException, java.io.IOException{
-        request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(
+        String name = request.getParameter("name")
+        String password = request.getParameter("password)"
+
+                boolean isUserValid = service.isUserValid(name,password);
+
+        if(isUserValid)
+        {request.setAttribute("name", name);
+        request.setAttribute("password", password);
+        request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(
                 request,response);
       )
+        }
+        else {
+            request.setAttribute("errorMessage", "invalidCreds")
+            request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(
+                    request,response);
+        }
     }
 
 }
